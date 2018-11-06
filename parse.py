a808284746b9c63fbbf6cb2efaf3a6399b931d1c
dayatura <hidayaturrahman7@gmail.com>
2018-11-06 22:17:42 +0700

    add some new rule

--- parse.py
+++ parse.py
@@ -57,8 +57,51 @@ def accept(T):
 # <hex_digit> ::= "0" … "9" | "A" … "F" | "a" … "f"
 
 # <identifier> ::= letter { letter | unicode_digit }
+
+def nt_identifier():
+	nt_letter()
+	while symbol in set(string.ascii_letters):
+		nt_letter()
+	while symbol in set(string.unicode_digit):
+		nt_unicode_digit()
+
 # <int_lit> ::= decimal_lit | octal_lit | hex_lit
+
+def nt_int_lit():
+	if symbol in set(string.digits):
+		nt_decimal_lit()
+	if symbol in set(string.octdigits):
+		nt_octal_lit()
+	if symbol in set(string.hexdigits):
+		nt_hex_lit()
+
 # <decimal_lit> ::= ( "1" … "9" ) { decimal_digit }
+
+def nt_decimal_lit():
+	if symbol == '1':
+		accept('1')
+	elif symbol == '2':
+		accept('2')
+	elif symbol == '3':
+		accept('3')
+	elif symbol == '4':
+		accept('4')
+	elif symbol == '5':
+		accept('5')
+	elif symbol == '6':
+		accept('6')
+	elif symbol == '7':
+		accept('7')
+	elif symbol == '8':
+		accept('8')
+	elif symbol == '9':
+		accept('9')
+	else:
+		output_error_and_halt()
+
+	while symbol in set(string.digits):
+		nt_decimal_digit()
+	
 # <octal_lit> ::= "0" { octal_digit }
 # <hex_lit> ::= "0" ( "x" | "X" ) hex_digit { hex_digit }
 # <float_lit> ::= decimals "." [ decimals ] [ exponent ] | decimals exponent | "." decimals [ exponent ]
@@ -67,6 +110,15 @@ def accept(T):
 # <imaginary_lit> ::= (decimals | float_lit) "i"
 
 # <rune_lit> ::= " ' " ( unicode_value | byte_value ) " ' "
+def nt_rune_lit():
+	accept("'")
+	if symbol in set(string.unicode_value):
+		nt_unicode_value()
+	elif symbol in set(string.hexdigits).union(string.octdigits):
+		nt_byte_value()
+	else:
+		output_error_and_halt()
+	accept("'")
 # <unicode_value> ::= unicode_char | little_u_value | big_u_value | escaped_char
 # <byte_value> ::= octal_byte_value | hex_byte_value
 # <octal_byte_value> ::= `\` octal_digit octal_digit octal_digit
@@ -78,6 +130,7 @@ def accept(T):
 # <string_lit> ::= raw_string_lit | interpreted_string_lit
 # <raw_string_lit> ::= "`" { unicode_char | newline } "`"
 # <interpreted_string_lit> ::= `"` { unicode_value | byte_value } `"`
+
 # <Type> ::= TypeName | TypeLit | "(" Type ")"
 # <TypeName> ::= identifier | QualifiedIdent
 # <TypeLit> ::= ArrayType | StructType | PointerType | FunctionType | InterfaceType | SliceType | MapType | ChannelType
