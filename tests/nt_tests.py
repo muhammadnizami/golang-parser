5be45be9eddc9470c6ea8d0b0f6c2fee0201fe82
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2018-11-12 09:47:38 +0700

    fix tests

--- tests/nt_tests.py
+++ tests/nt_tests.py
@@ -1,3 +1,5 @@
+# coding: utf8
+
 import os,sys
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
@@ -5,6 +7,7 @@ from parse import *
 
 def test_nt(nt_fun, input_file, should_halt):
 	global line_no, column_no
+	test_fail=False
 	try:
 		print("testing " + nt_fun.__name__ + " with input " + input_file )
 		open_symbolic_file(input_file)
@@ -13,14 +16,20 @@ def test_nt(nt_fun, input_file, should_halt):
 		if not should_halt:
 			print("SUCCESS: " + nt_fun.__name__ + " succeeded as expected")
 		else:
+			test_fail=True
 			print("FAIL: " + nt_fun.__name__ + " didn't halt as expected")
 	except SystemExit:
 		if should_halt:
 			print("SUCCESS: " + nt_fun.__name__ + " halted as expected")
 		else:
+			test_fail=True
 			print("FAIL: " + nt_fun.__name__ + " halted incorrectly")
-	print("line_no: " + str(line_no) + ", column_no: " + str(column_no))
+	except Exception as e:
+		print('Exception: ')
+		print(e)
+		test_fail=True
 	print()
+	return test_fail
 
 def accepta(): accept('a')
 def acceptb(): accept('b')
@@ -35,8 +44,16 @@ tests = [
 ]
 
 def main():
+	num_fail = 0
+	num_succ = 0
 	for nt_fun, input_file, should_halt in tests:
-		test_nt(nt_fun, input_file, should_halt)
+		test_fail = test_nt(nt_fun, input_file, should_halt)
+		if test_fail:
+			num_fail += 1
+		else:
+			num_succ += 1
+
+	print(num_fail, "tests failed, ", num_succ, "tests succeeded")
 
 if __name__ == "__main__":
 	main()
